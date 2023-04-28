6d8bc681487f22e4371c927197fce4be931321a3
aifel regunay <aifeljake7@gmail.com>
2023-04-28 11:55:02 +0800

    added activity s29

--- s29/activity/activity.js
+++ s29/activity/activity.js
@@ -1,4 +1,11 @@
-db.users.find({ $or: [ {firstName: "s"}, {lastName: "d"} ] });
+db.users.find({ $or: 
+	[{
+		firstName: {$regex: "s", $options: "i"}}, {lastName: {$regex: "d", $options: "i"}}]},
+	{
+		firstName: 1,
+		lastName: 1,
+		_id: 0
+	});
 
 db.users.find({ $and: [ {age: {$gte: 70}} ] });
 
